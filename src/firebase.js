2eab28a2248367271137b8e7a40668285388bf01
AlanNJ <alannpn75@gmail.com>
2022-01-09 23:36:42 +0530

    solved login issue

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import firebase from "firebase/compat";
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-	apiKey: `${process.env.REACT_APP_API_KEY}`,
-	authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-	projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-	storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
-	messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-	appId: `${process.env.REACT_APP_APP_ID}`,
-	measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`,
+	apiKey: "AIzaSyCIvv-rodCBnftynCj3HO9wWFnTwyGLapo",
+	authDomain: "linkedin-clone-18d64.firebaseapp.com",
+	projectId: "linkedin-clone-18d64",
+	storageBucket: "linkedin-clone-18d64.appspot.com",
+	messagingSenderId: "279890073037",
+	appId: "1:279890073037:web:6e0a364310e56385bce084",
+	measurementId: "G-RLSH5J8YQV",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
